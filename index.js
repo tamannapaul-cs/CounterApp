28d47ea792b6019bbe0b8113a1066b310f7c2119
Tamanna Paul <101959910+tamannapaul-cs@users.noreply.github.com>
2022-03-22 00:37:33 -0400

    Initial Commit for project

---
+++ index.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+function increment() {
+  let h1Tag = document.getElementById("passangerCount");
+  count = count + 1;
+  h1Tag.innerText = count;
+}
+
+console.log("Hello Agartala");
